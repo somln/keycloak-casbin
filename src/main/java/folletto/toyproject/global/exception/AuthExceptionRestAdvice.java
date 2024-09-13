615d20bd0e75f3005cf307bcff0736babf5927cf
Somin Park <ps4708@naver.com>
2024-09-13 20:48:37 +0900

    feat: Authorization관련 예외 처리 추가

---
+++ src/main/java/folletto/toyproject/global/exception/AuthExceptionRestAdvice.java
@@ -0,0 +1,28 @@
+package folletto.toyproject.global.exception;
+
+import folletto.toyproject.global.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = {"folletto.toyproject.domain.post.api", "folletto.toyproject.domain.comment.api"})
+public class AuthExceptionRestAdvice {
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ResponseDto<Void>> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ResponseDto.errorWithMessage(HttpStatus.UNAUTHORIZED, "Authentication failed: " + e.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ResponseDto<Void>> handleAccessDeniedException(AccessDeniedException e) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(ResponseDto.errorWithMessage(HttpStatus.FORBIDDEN, "Access denied: " + e.getMessage()));
+    }
+
+}
